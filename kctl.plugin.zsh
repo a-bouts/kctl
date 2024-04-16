4e7f3f345e043520da68d3bf56b6800895a5b7e4
Arnaud Bouts <arnaud.bouts@orange.com>
2024-04-16 16:45:42 +0200

    use delete-emptydir-data as delete-local-data is deprecated

--- kctl.plugin.zsh
+++ kctl.plugin.zsh
@@ -271,7 +271,7 @@ function kpflpo() {
 
 # DRAIN
 function kdrain() {
-  kc drain $1 --timeout=300s --ignore-daemonsets --force --delete-local-data || kc drain $1 --timeout=60s --disable-eviction --ignore-daemonsets --force --delete-local-data
+  kc drain $1 --timeout=300s --ignore-daemonsets --force --delete-emptydir-data || kc drain $1 --timeout=60s --disable-eviction --ignore-daemonsets --force --delete-emptydir-data
 }
 
 
