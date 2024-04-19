9d47f5d975f477e0302494eb2a948ebef1c22b8f
a-bouts <arnaud.bouts@gmail.com>
2024-04-19 21:06:08 +0200

    init autocompletion

--- kctl.plugin.zsh
+++ kctl.plugin.zsh
@@ -2,6 +2,22 @@
 #
 #
 #
+
+if (( ! $+commands[kubectl] )); then
+  return
+fi
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `kubectl`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _kubectl
+  _comps[kubectl]=_kubectl
+fi
+
+#kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
+
+
 function ksetns() {
   ${KCTL_BINARY} config set-context --current --namespace=$1
   _kctl_get_ns
@@ -23,6 +39,14 @@ function ksetctx() {
 function kctx() {
   _kctl_use_context $1
 }
+
+_kctx() {
+  words=("kubectl" "config" "use-context" "${(@)words[2,$#words]}")
+  CURRENT=$#words
+  _kubectl
+}
+
+compdef _kctx kctx
 
 kc() {
   echo "+ ${KCTL_BINARY} $@">&2;
