0b1a37d13a7fe6416defd58e2c1df48c1bd3e174
Arnaud Bouts <arnaud.bouts@orange.com>
2024-04-30 14:25:51 +0200

    add flux aliases

--- kctl.plugin.zsh
+++ kctl.plugin.zsh
@@ -17,13 +17,15 @@ fi
 
 kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
 
-__kubectl_debug()
-{
-    local file="$BASH_COMP_DEBUG_FILE"
-    if [[ -n ${file} ]]; then
-        echo "$*" >> "${file}"
-    fi
-}
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `flux`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_flux" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _flux
+  _comps[flux]=_flux
+fi
+
+flux completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_flux" &|
 
 function ksetns() {
   ${KCTL_BINARY} config set-context --current --namespace=$1
@@ -36,7 +38,6 @@ function kns() {
 }
 
 _kns() {
-  __kubectl_debug "$(_k_with_context)"
   words=("kubectl" "${$(_k_with_context)[@]}" "get" "namespace" "${(@)words[2,$#words]}")
   CURRENT=$#words
   _kubectl
@@ -67,8 +68,15 @@ compdef _kctx kctx
 compdef _kctx ksetctx
 
 kc() {
-  echo "+ ${KCTL_BINARY} $@">&2;
+  echo "\033[0;33m>\033[0m \033[1;30m${KCTL_BINARY} $@\033[0m">&2;
   command ${KCTL_BINARY} $@;
+  echo "\033[0;33m<\033[0m \033[1;30m${KCTL_BINARY} $@\033[0m">&2;
+}
+
+_flx() {
+  echo "\033[0;33m>\033[0m \033[1;30m${FLUX_BINARY} $@\033[0m">&2;
+  command ${FLUX_BINARY} $@;
+  echo "\033[0;33m<\033[0m \033[1;30m${FLUX_BINARY} $@\033[0m">&2;
 }
 
 k_with_namespace() {
@@ -117,6 +125,141 @@ _k() {
 }
 
 compdef _k k
+
+
+f_with_namespace() {
+  if [[ "$@" =~ "^(.* )?-n( .*)?$" || "$@" =~ "^(.* )?--namespace( .*)?$" || "$@" =~ "^(.* )?-A( .*)?$" || "$KCTL_USE_NAMESPACE" == "flux-system" ]];
+  then
+    f_with_context $@;
+  else
+    f_with_context -n $KCTL_USE_NAMESPACE $@;
+  fi
+}
+
+_f_with_namespace() {
+  if [[ ${words[(ie)"-n"]} -lt ${#words} || ${words[(ie)"--namespace"]} -lt ${#words} || ${words[(ie)"-A"]} -lt ${#words} || "$KCTL_USE_NAMESPACE" == "flux-system" ]];
+  then
+  else
+    echo "--namespace" "$KCTL_USE_NAMESPACE"
+  fi
+}
+
+f_with_context() {
+  if [[ "$@" =~ "^(.* )?--context( .*)?$" || "$KCTL_USE_CONTEXT" == "$KCTL_CONTEXT" || -z "$KCTL_USE_CONTEXT" ]];
+  then
+    _flx $@;
+  else
+    _flx --context $KCTL_USE_CONTEXT $@;
+  fi
+}
+
+_f_with_context() {
+  if [[ ${words[(ie)"--context"]} -lt ${#words} || "$KCTL_USE_CONTEXT" == "$KCTL_CONTEXT" || -z "$KCTL_USE_CONTEXT" ]];
+  then
+  else
+    echo "--context" "$KCTL_USE_CONTEXT"
+  fi
+}
+
+fx() {
+  f_with_namespace $@
+}
+
+_fx() {
+  params=("${(@)words[2,$#words]}")
+  words=("flux" "${$(_f_with_context)[@]}" "${$(_f_with_namespace)[@]}" "${params[@]}")
+  CURRENT=$#words
+  _flux
+}
+
+compdef _fx fx
+
+# flux get
+alias fxg='fx get'
+alias fxgap='fxg alert-providers'
+alias fxga='fxg alerts'
+alias fxgall='fxg all'
+alias fxghr='fxg helmreleases'
+alias fxgim='fxg images'
+alias fxgkz='fxg kustomizations'
+alias fxgr='fxg receivers'
+
+alias fxgnok='fx get --status-selector ready=false'
+
+
+alias fxgsrc='fxg sources all'
+alias fxgchart='fxg sources chart'
+alias fxggit='fxg sources git'
+alias fxghrepo='fxg sources helm'
+alias fxgoci='fxg sources oci'
+
+# flux delete
+alias fxrm='fx delete'
+alias fxrmap='fxrm alert-provider'
+alias fxrma='fxrm alert'
+alias fxrmhr='fxrm helmrelease'
+alias fxrmim='fxrm image'
+alias fxrmkz='fxrm kustomization'
+alias fxrmr='fxrm receiver'
+
+# flux events
+alias fxe='fx events'
+
+# flux logs
+alias fxl='f_with_context logs'
+
+# flux reconcile
+alias fxrec='fx reconcile'
+alias fxrechr='fxrec helmrelease'
+alias fxrecim='fxrec image'
+alias fxreckz='fxrec kustomization'
+alias fxrecr='fxrec receiver'
+
+alias fxrecsrc='fxrec sources'
+alias fxrecchart='fxrecsrc chart'
+alias fxrecgit='fxrecsrc git'
+alias fxrechrepo='fxrecsrc helm'
+alias fxrecoci='fxrecsrc oci'
+
+# flux resume
+alias fxres='fx resume'
+alias fxresap='fxres alert-provider'
+alias fxresa='fxres alert'
+alias fxreshr='fxres helmrelease'
+alias fxresim='fxres image'
+alias fxreskz='fxres kustomization'
+alias fxresr='fxres receiver'
+
+alias fxressrc='fxres sources'
+alias fxreschart='fxres sources chart'
+alias fxresgit='fxres sources git'
+alias fxreshrepo='fxres sources helm'
+alias fxresoci='fxres sources oci'
+
+# flux suspend
+alias fxsus='fx suspend'
+alias fxsusap='fxsus alert-provider'
+alias fxsusa='fxsus alert'
+alias fxsushr='fxsus helmrelease'
+alias fxsusim='fxsus image'
+alias fxsuskz='fxsus kustomization'
+alias fxsusr='fxsus receiver'
+
+alias fxsussrc='fxsus sources'
+alias fxsuschart='fxsus sources chart'
+alias fxsusgit='fxsus sources git'
+alias fxsushrepo='fxsus sources helm'
+alias fxsusoci='fxsus sources oci'
+
+# flux stats
+alias fxst='fx stats'
+
+# flux trace
+alias fxtr='fx trace'
+
+# flux tree
+alias fxt='fx tree kustomization'
+
 
 # GET
 alias kg='k get'
@@ -441,12 +584,15 @@ KCTL_SUFFIX="${KCTL_SUFFIX-) }"
 KCTL_SYMBOL_COLOR="${KCTL_SYMBOL_COLOR-12}"
 KCTL_CTX_COLOR="${KCTL_CTX_COLOR-red}"
 KCTL_NS_COLOR="${KCTL_NS_COLOR-cyan}"
+KCTL_TIP_COLOR="${KCTL_TIP_COLOR-yellow}"
 KCTL_BG_COLOR="${KCTL_BG_COLOR}"
 KCTL_KUBECONFIG_CACHE="${KUBECONFIG}"
 KCTL_DISABLE_PATH="${HOME}/.kube/kctl/disabled"
 KCTL_LAST_TIME=0
 KCTL_CLUSTER_FUNCTION="${KCTL_CLUSTER_FUNCTION}"
 KCTL_NAMESPACE_FUNCTION="${KCTL_NAMESPACE_FUNCTION}"
+
+FLUX_BINARY="${FLUX_BINARY:-flux}"
 
 # Determine our shell
 if [ "${ZSH_VERSION-}" ]; then
@@ -724,7 +870,7 @@ EOF
 
 kctlon() {
   if [[ "${1}" == '-h' || "${1}" == '--help' ]]; then
-    _kubeon_usage
+    _kctlon_usage
   elif [[ "${1}" == '-g' || "${1}" == '--global' ]]; then
     rm -f -- "${KCTL_DISABLE_PATH}"
   elif [[ "$#" -ne 0 ]]; then
@@ -738,7 +884,7 @@ kctlon() {
 
 kctloff() {
   if [[ "${1}" == '-h' || "${1}" == '--help' ]]; then
-    _kubeoff_usage
+    _kctloff_usage
   elif [[ "${1}" == '-g' || "${1}" == '--global' ]]; then
     mkdir -p -- "$(dirname "${KCTL_DISABLE_PATH}")"
     touch -- "${KCTL_DISABLE_PATH}"
